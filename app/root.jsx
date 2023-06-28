064ded7439f4152964da070e113849b93d6f1a9b
lucasCardozo <lucasivancardozo27@gmial.com>
2023-06-27 23:48:31 -0300

    mejora de rendimiento

--- app/root.jsx
+++ app/root.jsx
@@ -74,7 +74,10 @@ export default function App() {
     <html lang="es">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1, minimum-scale=1"
+        />
         <Meta />
         <Links />
       </head>
