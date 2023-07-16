9e764820099d755b7038bd8cff6537c6cafaa96b
lucasCardozo <lucasivancardozo27@gmial.com>
2023-07-16 17:10:03 -0300

    arreglo de imagenes en footer que estaban sin link

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -7,13 +7,31 @@ export default function Footer() {
     <footer className="footer">
       <ul className="footer_ul" id="contactos">
         <li className="footer_li">
-          <img className="footer_img" src={instagram} alt="" />
+          <a
+            href="https://www.instagram.com/elbowlingdepaso/'"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="footer_img" src={instagram} alt="" loading="lazy" />
+          </a>
         </li>
         <li className="footer_li">
-          <img className="footer_img" src={facebook} alt="" />
+          <a
+            href="https://www.facebook.com/elbowlingdepaso2442"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="footer_img" src={facebook} alt="" loading="lazy" />
+          </a>
         </li>
         <li className="footer_li">
-          <img className="footer_img" src={whatsapp} alt="" />
+          <a
+            href="https://api.whatsapp.com/send?phone=542235368630&text=Hola!%20Quer%C3%ADa%20hacer%20una%20reserva%20para%20jugar%20al%20bowling!%F0%9F%8E%B3%F0%9F%8E%B3"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="footer_img" src={whatsapp} alt="" loading="lazy" />
+          </a>
         </li>
       </ul>
       <p className="footer_socialText">
