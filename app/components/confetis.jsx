0ea55bee04152db9fc20230e670bfeddaacb7686
lucasCardozo <lucasivancardozo27@gmial.com>
2023-07-19 17:49:23 -0300

    mejora en el rendimiento de los confetis

--- app/components/confetis.jsx
+++ app/components/confetis.jsx
@@ -2,11 +2,12 @@ export default function Confetis({ direction }) {
   const colors = ['red', 'green', 'blue', 'orange', 'yellow'];
 
   const getRandomStyles = () => {
-    let num = 1 + Math.floor(Math.random() * 10);
-    let time = 1 + Math.random() * 1.5;
+    let num = 1 + Math.floor(Math.random().toFixed(2) * 10);
+    let time = 1 + Math.random().toFixed(2) * 1.5;
     return {
-      backgroundColor: `${colors[Math.floor(Math.random() * colors.length)]}`,
-      top: `${50 + Math.floor(Math.random() * 25)}%`,
+      backgroundColor:
+        colors[Math.floor(Math.random().toFixed(2) * colors.length)],
+      top: `${50 + Math.floor(Math.random().toFixed(2) * 25)}%`,
       animation: `${time}s ease-out .5s x${num} forwards`,
     };
   };
@@ -16,7 +17,6 @@ export default function Confetis({ direction }) {
       className="confetis"
       style={direction ? { transform: 'rotateY(180deg)' } : null}
     >
-      <div className="confeti" style={getRandomStyles()}></div>
       <div className="confeti" style={getRandomStyles()}></div>
       <div className="confeti" style={getRandomStyles()}></div>
       <div className="confeti" style={getRandomStyles()}></div>
