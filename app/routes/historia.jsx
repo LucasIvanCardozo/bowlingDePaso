a4518614160d0fafcf68582c76227e67bf97fa87
lucasCardozo <lucasivancardozo27@gmial.com>
2023-08-07 14:03:50 -0300

    mejora en texto de historia

--- app/routes/historia.jsx
+++ app/routes/historia.jsx
@@ -62,14 +62,15 @@ export default function Historia() {
             <div className="intro_data">
               <h2 className="intro_title">14 de Septiembre 1988</h2>
               <p className="intro_text">
-                ¡El comienzo de una leyenda! En la emblemática Avenida Paso y
-                Santa Fe, dimos vida a nuestro querido bowling. Aquel día,
-                marcado en la historia, inauguramos un espacio de diversión sin
-                igual. Desde entonces, hemos sido el punto de encuentro para
-                amantes del bowling de todas las edades. Nuestro compromiso
-                inquebrantable es brindar experiencias excepcionales y momentos
-                inolvidables. ¡Únete a nosotros y forma parte de nuestra
-                historia en constante crecimiento!
+                ¡El comienzo de una leyenda! En la emblemática Avenida{' '}
+                <b>Paso y Santa Fe</b>, dimos vida a nuestro querido{' '}
+                <b>bowling</b>. Aquel día, marcado en la historia, inauguramos
+                un espacio de diversión sin igual. Desde entonces, hemos sido el
+                punto de encuentro para amantes del <b>bowling</b> de todas las
+                edades. Nuestro compromiso inquebrantable es brindar
+                experiencias excepcionales y momentos inolvidables. ¡Únete a
+                nosotros y forma parte de nuestra historia en constante
+                crecimiento!
               </p>
               <img className="intro_img" src={inauguracion} alt="" />
             </div>
@@ -79,13 +80,13 @@ export default function Historia() {
             <div className="intro_data">
               <h2 className="intro_title">8 de Agosto 2022</h2>
               <p className="intro_text">
-                Cerramos temporalmente nuestro querido bowling debido a desafíos
-                económicos, una decisión difícil y devastadora para nuestro
-                equipo y comunidad de jugadores. A pesar de ello, estamos
-                comprometidos en encontrar soluciones y reabrir en un futuro
-                cercano. Agradecemos el apoyo y comprensión durante este periodo
-                difícil y esperamos recibirlos nuevamente para crear momentos
-                inolvidables juntsvg
+                <b>Cerramos temporalmente</b> nuestro querido bowling debido a
+                desafíos económicos, una decisión difícil y devastadora para
+                nuestro equipo y comunidad de jugadores. A pesar de ello,
+                estamos comprometidos en encontrar soluciones y reabrir en un
+                futuro cercano. Agradecemos el apoyo y comprensión durante este
+                periodo difícil y esperamos recibirlos nuevamente para crear
+                momentos inolvidables juntos.
               </p>
               <img className="intro_img" src={cierre} alt="" />
             </div>
@@ -95,13 +96,14 @@ export default function Historia() {
             <div className="intro_data">
               <h2 className="intro_title">10 de Marzo 2023</h2>
               <p className="intro_text">
-                ¡Estamos de vuelta y en un lugar mejor! Después de superar
-                desafíos económicos, hemos reabierto en Cordoba 2866. Este nuevo
-                espacio nos ofrece más oportunidades para brindar una
-                experiencia excepcional de bowling. Esperamos dar la bienvenida
-                a nuestros jugadores y a la comunidad, creando recuerdos
-                inolvidables en nuestro renovado establecimiento. ¡Los esperamos
-                con entusiasmo en nuestro emocionante nuevo lugar!
+                ¡<b>Estamos de vuelta y en un lugar mejor!</b> Después de
+                superar desafíos económicos, hemos reabierto en{' '}
+                <b>Cordoba 2866</b>. Este nuevo espacio nos ofrece más
+                oportunidades para brindar una experiencia excepcional de
+                bowling. Esperamos dar la bienvenida a nuestros jugadores y a la
+                comunidad, creando recuerdos inolvidables en nuestro renovado
+                establecimiento. ¡Los esperamos con entusiasmo en nuestro
+                emocionante nuevo lugar!
               </p>
               <img className="intro_img" src={abrimos} alt="" />
             </div>
